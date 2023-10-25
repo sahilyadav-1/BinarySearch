1bd5e03122191de7bdb10c3396da0bd8ba883964
sahilyadav-1 <121487105+sahilyadav-1@users.noreply.github.com>
2023-10-26 00:04:58 +0530

    Update EkoEko.cpp

--- EkoEko.cpp
+++ EkoEko.cpp
@@ -6,43 +6,35 @@ using namespace std;
 
 bool ispossiblesoln(vector<int>& arr, int n, int m, int target){
     int sum = 0;
-
-    for(int i=0;i<n;i++){
-        int diff = 0;
-        if(arr[i] > m)
-            diff = arr[i]-m;
-        sum = sum + diff;
+    for(int i=0; i<n; i++){
+        if(arr[i]>m){
+            int diff = arr[i]-m;
+            sum += diff;
+        }
+        if(sum >= target){
+            return true;
+        }
     }
-    
-    if(sum >= target)
-        return true;
-    else
-        return false;
+    return false;
 }
-
 int getMaximumHeightOfSaw(vector<int>& arr, int n, int target){
     int s = 0;
-    int maxi = INT_MIN;
-    for(int i = 0; i<n; i++)
-        maxi = max(maxi,arr[i]);
-    int e = maxi;
-    int m = s + (e-s)/2;
-    int ans = -1;
+    int e = arr[n-1];
 
+    int m = s+(e-s)/2;
+    int ans = -1;
     while(s<=e){
-        if(ispossiblesoln(arr,n,m,target))
-        {
+        if(ispossiblesoln(arr,n,m,target)){
             ans = m;
             s = m+1;
         }
         else{
             e = m-1;
         }
-        m = s + (e-s)/2;
+        m = s+(e-s)/2;
     }
     return ans;
 }
-
 
 int main(){
     int target;
